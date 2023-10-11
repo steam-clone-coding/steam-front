36ce258644ace63535dfaccd66219bdcf15f90b0
OhSeChan <seb_s19@naver.com>
2023-10-11 15:34:36 +0900

    :sparkles: LoginPage, LoginForm 생성
    - 로그인 페이지와 로그인 폼을 생성

---
+++ steam-front/src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginForm from './components/LoginForm';
+import './styles/LoginPage.css';
+
+const LoginPage = () => {
+  return (
+    <div className='login'>
+      Login Page
+      <LoginForm/>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ steam-front/src/pages/LoginPage/components/LoginForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/LoginForm.css';
+
+const LoginForm = () => {
+  return (
+    <div className='loginform'>
+      loginForm
+    </div>
+  );
+};
+
+export default LoginForm;
