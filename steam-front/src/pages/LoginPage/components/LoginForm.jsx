8dadfe9eb3fcb5cd8a9792a666f83d9f62259b07
OhSeChan <seb_s19@naver.com>
2023-10-11 20:42:09 +0900

    :lipstick: 로그인 폼 스타일 추가 및 checkbox 변경
    - 로그인 폼 스타일 추가
    - checkbox를 input이 아닌 div로 변경한 뒤 체크를 svg로 변경
    - 체크 동작은 이후 추가 예정

--- steam-front/src/pages/LoginPage/components/LoginForm.jsx
+++ steam-front/src/pages/LoginPage/components/LoginForm.jsx
@@ -15,7 +15,11 @@ const LoginForm = () => {
           <input type='password'></input>
         </label>
         <label>
-          <input type='checkbox'></input>
+          <div className='loginform_checkbox'>
+            <div className='loginform_checkbox_check'>
+              <svg version="1.1" id="base" xmlns="http://www.w3.org/2000/svg" class="SVGIcon_Button SVGIcon_Check" x="0px" y="0px" width="256px" height="256px" viewBox="0 0 256 256" stroke-width="35" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"><polyline fill="none" points="49.5,147.75 95,210.75 206.5,45.25 "></polyline></svg>
+            </div>
+          </div>
           로그인 정보 저장
         </label>
         <button type='submit' value="Submit">로그인</button>
