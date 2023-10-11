79e9cefcb12c99e6f272fe198e653b39a474dad5
OhSeChan <seb_s19@naver.com>
2023-10-11 19:30:34 +0900

    :sparkles: 로그인 페이지 기본 구조 추가

--- steam-front/src/pages/LoginPage/LoginPage.jsx
+++ steam-front/src/pages/LoginPage/LoginPage.jsx
@@ -3,10 +3,20 @@ import LoginForm from './components/LoginForm';
 import './styles/LoginPage.css';
 
 const LoginPage = () => {
+
   return (
     <div className='login'>
       Login Page
       <LoginForm/>
+      <div className='login_bottom'>
+        <div className='login_bottom_signup'>
+          <p>Steam에 처음 오셨나요?</p>
+          <button>가입하기</button>
+        </div>
+        <div>
+          <p>무료로 쉽게 가입할 수 있습니다. 수천 종류의 게임을 전 세계 새로운 친구들과 힘께 즐겨보세요.</p>
+        </div>
+      </div>
     </div>
   );
 };
--- steam-front/src/pages/LoginPage/components/LoginForm.jsx
+++ steam-front/src/pages/LoginPage/components/LoginForm.jsx
@@ -2,9 +2,24 @@ import React from 'react';
 import '../styles/LoginForm.css';
 
 const LoginForm = () => {
+
   return (
     <div className='loginform'>
-      loginForm
+      <form>
+        <label>
+          계정 이름으로 로그인
+          <input type='text'></input>
+        </label>
+        <label>
+          비밀번호
+          <input type='password'></input>
+        </label>
+        <label>
+          <input type='checkbox'></input>
+          로그인 정보 저장
+        </label>
+        <button type='submit' value="Submit">로그인</button>
+      </form>
     </div>
   );
 };
