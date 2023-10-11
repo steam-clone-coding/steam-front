f1231dab262d4b944ac3e645604425c6d40a3d26
OhSeChan <seb_s19@naver.com>
2023-10-11 15:54:51 +0900

    :sparkles: 라우팅 추가
    - Contents 클래스를 가지는 div 하위에 라우팅을 임시로 추가, 이후 수정 필요
    - /login path로 LoginPage 컴포넌트 라우팅

--- steam-front/src/App.jsx
+++ steam-front/src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import { Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage/MainPage';
+import LoginPage from './pages/LoginPage/LoginPage';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <div className="Contents" style={{height:"100vh"}}>
-        ㅎㅇㅎㅇ
+        <Routes>
+          <Route path='/' element={<MainPage/>}/>
+          <Route path='/login' element={<LoginPage/>}/>
+        </Routes>
       </div>
       <Footer/>
     </div>
