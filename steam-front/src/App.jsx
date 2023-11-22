8e255ccab1bc00443a27f082edd26786534191ce
LeeTaeHeon <forever296@naver.com>
2023-11-22 20:53:51 +0900

    feat: 메인페이지 캐로셀 기능완료 (디자인 미구현)

--- steam-front/src/App.jsx
+++ steam-front/src/App.jsx
@@ -1,16 +1,22 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage/MainPage';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <div className="Contents" style={{height:"100vh"}}>
-        ㅎㅇㅎㅇ
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <div className="Contents" style={{ height: "100vh" }}>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer/>
-    </div>
+    </BrowserRouter>
   );
 }
 
